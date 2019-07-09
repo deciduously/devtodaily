937a148009574fbfc51cf012cacc9e30b2cdcb1c
deciduously <ben@deciduously.com>
2019-07-09 19:36:20 -0400

    Add header

--- 1/trimAtBothEnds.hs
+++ 1/trimAtBothEnds.hs
@@ -1,2 +1,5 @@
+-- Removes the first and lest letter of a string over 2 characters long
+-- https://dev.to/thepracticaldev/daily-challenge-1-string-peeler-4nep
+
 trimAtBothEnds :: String -> Maybe String
 trimAtBothEnds s = if ((length s) <= 2) then Nothing else Just (init.tail $ s)
